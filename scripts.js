d9f6a0a9e7320944a4ec9df8f5a25366698b0036
Armin Ebner <armin_ebner@web.de>
2020-11-06 17:23:14 +0100

    finished cards animation and share-function for single-page. multi-page option in the making.

--- scripts.js
+++ scripts.js
@@ -60,32 +60,84 @@ const closeModal = document.querySelectorAll('.share-modal__close')
 
 function disappearModal(index) {
     setTimeout(() => { 
-        console.log(index);
         modals[index].classList.remove('share-modal--visible')
-        }, 5000)
+        }, 10000)
 }
 
 icons.forEach(icon => {
     icon.addEventListener('click', () => {
        if(icon.classList.contains('share-modal__one')) {
-           console.log('entered');
-           modals[0].classList.add('share-modal--visible')
-           disappearModal(0);
+           let recipeLink = window.location.href
+           modals[0].firstElementChild.nextElementSibling.nextElementSibling.value = recipeLink
+           if(icon.classList.contains('instagram')) {
+                modals[0].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/instagram.svg"><p>Instagramm</p>';
+            }else if(icon.classList.contains('facebook')) {
+                modals[0].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/facebook.svg"><p>Facebook</p>';
+            }else if(icon.classList.contains('twitter')) {
+                modals[0].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/twitter.svg"><p>Twitter</p>';
+            }
+            modals[0].classList.add('share-modal--visible')
+            disappearModal(0);
        }else if(icon.classList.contains('share-modal__two')) {
-        modals[1].classList.add('share-modal--visible')
-        disappearModal(1);
+            let recipeLink = window.location.href
+            modals[1].firstElementChild.nextElementSibling.nextElementSibling.value = recipeLink
+            if(icon.classList.contains('instagram')) {
+                modals[1].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/instagram.svg"><p>Instagramm</p>';
+            }else if(icon.classList.contains('facebook')) {
+                modals[1].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/facebook.svg"><p>Facebook</p>';
+            }else if(icon.classList.contains('twitter')) {
+                modals[1].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/twitter.svg"><p>Twitter</p>';
+            }
+            modals[1].classList.add('share-modal--visible')
+            disappearModal(1);
        }else if(icon.classList.contains('share-modal__three')) {
-        modals[2].classList.add('share-modal--visible')
-        disappearModal(2);
+            let recipeLink = window.location.href
+            modals[2].firstElementChild.nextElementSibling.nextElementSibling.value = recipeLink
+            if(icon.classList.contains('instagram')) {
+                modals[2].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/instagram.svg"><p>Instagramm</p>';
+            }else if(icon.classList.contains('facebook')) {
+                modals[2].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/facebook.svg"><p>Facebook</p>';
+            }else if(icon.classList.contains('twitter')) {
+                modals[2].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/twitter.svg"><p>Twitter</p>';
+            }
+            modals[2].classList.add('share-modal--visible')
+            disappearModal(2);
        }else if(icon.classList.contains('share-modal__four')) {
-        modals[3].classList.add('share-modal--visible')
-        disappearModal(3);
+            let recipeLink = window.location.href
+            modals[3].firstElementChild.nextElementSibling.nextElementSibling.value = recipeLink
+            if(icon.classList.contains('instagram')) {
+                modals[3].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/instagram.svg"><p>Instagramm</p>';
+            }else if(icon.classList.contains('facebook')) {
+                modals[3].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/facebook.svg"><p>Facebook</p>';
+            }else if(icon.classList.contains('twitter')) {
+                modals[3].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/twitter.svg"><p>Twitter</p>';
+            }
+            modals[3].classList.add('share-modal--visible')
+            disappearModal(3);
        }else if(icon.classList.contains('share-modal__five')) {
-        modals[4].classList.add('share-modal--visible')
-        disappearModal(4);
+            let recipeLink = window.location.href
+            modals[4].firstElementChild.nextElementSibling.nextElementSibling.value = recipeLink
+            if(icon.classList.contains('instagram')) {
+                modals[4].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/instagram.svg"><p>Instagramm</p>';
+            }else if(icon.classList.contains('facebook')) {
+                modals[4].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/facebook.svg"><p>Facebook</p>';
+            }else if(icon.classList.contains('twitter')) {
+                modals[4].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/twitter.svg"><p>Twitter</p>';
+            }
+            modals[4].classList.add('share-modal--visible')
+            disappearModal(4);
        }else if(icon.classList.contains('share-modal__six')) {
-        modals[5].classList.add('share-modal--visible')
-        disappearModal(5);
+            let recipeLink = window.location.href
+            modals[5].firstElementChild.nextElementSibling.nextElementSibling.value = recipeLink
+            if(icon.classList.contains('instagram')) {
+                modals[5].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/instagram.svg"><p>Instagramm</p>';
+            }else if(icon.classList.contains('facebook')) {
+                modals[5].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/facebook.svg"><p>Facebook</p>';
+            }else if(icon.classList.contains('twitter')) {
+                modals[5].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = '<img src="./img/armin/twitter.svg"><p>Twitter</p>';
+            }
+            modals[5].classList.add('share-modal--visible')
+            disappearModal(5);
        }
     })
 })
@@ -99,8 +151,7 @@ closeModal.forEach(x => {
     })
 })
 
-
-
+/*GET DYNAMIC SHARE-SERVICE AND LINK*/
 
 
 
